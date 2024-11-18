478f1c51ed9610263ab30a20e4e8d76d29bde946
MatiSarbo19 <matiassarbo19@gmail.com>
2024-11-18 03:19:38 -0300

    primer commit del tpi lab3

---
+++ TpiLab3-master/servicios/ventas-servicios.js
@@ -0,0 +1,70 @@
+const url = "https://672a9c2a976a834dd023e85f.mockapi.io/api/tpilab3/ventas";
+
+
+async function listar(id) {
+    let cadUrl;
+    if(isNaN(id))
+      cadUrl= url;
+    else 
+      cadUrl = url + "/" + id;  
+    return await fetch(cadUrl)
+        .then(respuesta => respuesta.json());
+}
+
+async function crear(idUsuario, emailUsuario, idProducto, nombreProducto, cantidad, fecha, despachado) {
+
+    return await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            idUsuario: idUsuario, 
+            emailUsuario: emailUsuario, 
+            idProducto: idProducto, 
+            nombreProducto: nombreProducto, 
+            cantidad: cantidad, 
+            fecha: fecha, 
+            despachado: despachado
+        })
+    })
+}
+
+async function editar(id,  despachado) {
+
+    let urlPut = url + "/" + id;
+    return await fetch(urlPut, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            despachado: despachado
+        })
+    })
+}
+
+async function listarVentasDespachadas(despachadas) {
+    const newUrl= new URL(url);
+    newUrl.searchParams.append('despachado', despachadas);
+    return await fetch(newUrl)
+        .then(respuesta => respuesta.json());
+ 
+}
+
+async function borrar(id){
+  
+    let urlPut = url + "/" + id;
+    return await fetch(urlPut, {
+            method: 'DELETE'
+       })
+}
+
+
+export const ventasServices = {
+    listar,
+    crear,
+    editar,
+    borrar,
+    listarVentasDespachadas
+}
